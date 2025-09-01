212fe30594d105f96355e503db0b87329e69da23
ruaa.abdel01 <ruaa.abdel01@gmail.com>
2025-08-31 21:13:30 -0400

    cleaned up folder

--- JavaScript/My-Codes/gscript.js
+++
@@ -1,9 +0,0 @@
-let correctNumber = 7;
-let guess = prompt("Guess a number between 1 and 10");
-
-while (parseInt(guess) !== correctNumber) {
-    guess = prompt("Wrong guess! Try again. Guess a number between 1 and 10");
-}
-
-alert("Congratulations! You've guessed the correct number: " + correctNumber);
-console.log("Game Over! The correct number was: " + correctNumber);
